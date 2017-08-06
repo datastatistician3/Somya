37b5846367b4069892764d4c8cc570e8f49bef82
Som Bohora <som-bohora@ouhsc.edu>
2017-08-06 15:16:56 -0500

    reset shinyjs

--- inst/shiny/shinyjs-practice-som.R
+++ inst/shiny/shinyjs-practice-som.R
@@ -3,6 +3,7 @@ library(shinyjs)
 
 ui <- fluidPage(
   useShinyjs(),
+  hidden(
   div(
   id = "reset_form",
   textInput("name", "Name", "Dean"),
@@ -10,19 +11,32 @@ ui <- fluidPage(
   checkboxInput("terms", "I agree", FALSE),
   actionButton("submit", "Submit"),
   actionButton("reset", "Reset Form")
+  # actionButton("btn", "Click me"),
+  # hidden(
+  #   p(id = "element", "I was born invisible")
+  #
+  # )
   )
+  ),
+  actionButton("hi", "Show")
 )
 
 
 server <- function(input, output, session){
+
+
   observe({
     toggleState("submit", condition = input$terms)
   })
-  
+
+  observeEvent(input$hi, {
+    show("reset_form")
+  })
+
   observeEvent(input$submit, {
     alert("Thank You!")
   })
-  
+
   observeEvent(input$reset, {
     reset("reset_form")
   })

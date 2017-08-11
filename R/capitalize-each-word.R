60cf2307e724ee8251a1388e776536f812c3b52f
Som Bohora <som-bohora@ouhsc.edu>
2017-08-11 10:45:51 -0500

    add capitalize each word

---
+++ R/capitalize-each-word.R
@@ -0,0 +1,9 @@
+capitalize_each_word <- function(x){
+  g <- l <- list()
+  g <- stringr::str_split(x, pattern = "[_| ]")
+  for ( i in 1:length(g)){
+    l[[i]] <- paste(Hmisc::capitalize(g[[i]]), collapse = " ")
+    j[[i]] <- paste(l[[i]], collapse = " ")
+}
+  return(j)
+}

8e902045a4e071312b23d0026e7e51d13dfcebd9
Som Bohora <som-bohora@ouhsc.edu>
2018-02-13 15:39:45 -0600

    underscore names

--- R/postfix-calculator.R
+++ R/postfix-calculator.R
@@ -3,16 +3,16 @@
 
 check_valid_operators <- function(input_string){
 	# only allow valid four operators
-	validChars = grepl("/|-|\\+|\\*|/|%", input_string)
+	valid_chars = grepl("/|-|\\+|\\*|/|%", input_string)
 
-	if (!validChars) {
+	if (!valid_chars) {
 	  # throw an error if one of four valid operators are not entered.
 		stop("Invalid expression. Please enter valid postfix expression.")
 	}
 }
 
 # Function to check if the character value is +, -, *, /.
-checkOperator <- function(char){
+check_operator <- function(char){
 	if (char == "+" | char == "-" | char == "%%" | char == "/" | char == "*" ){
 	  return(TRUE)
 	} else {
@@ -21,7 +21,7 @@ checkOperator <- function(char){
 }
 
 # Function to check whethern character is digit inbetween 0 to 9 or not.
-checkDigit <- function(x){
+check_digit <- function(x){
 	if (x >= '0')
 	{
 		return(TRUE)
@@ -31,7 +31,7 @@ checkDigit <- function(x){
 }
 
 # Function to execute tasks 
-executeTasks <- function(z, x, y){
+perform_operation <- function(z, x, y){
   if (z == "+") {
     return(x + y)
   } else if (z == "-") {
@@ -52,7 +52,7 @@ calculate_postfix <- function(input_str){
   
   check_valid_operators(input_string = input_str)
 
-	storeVector <- as.integer(c())
+	store_vector <- as.integer(c())
 	s <- unlist(strsplit(base::trimws(input_str), " "))
 	
 	check_illegal_char <- grepl("^[[:punct:]]\\d+|^\\d+[[:punct:]]|^[[:punct:]][[:punct:]]$", s)
@@ -63,42 +63,42 @@ calculate_postfix <- function(input_str){
 	
 	for (i in 1:length(s)) {
 		# check whether the operator is valid or not, and perform the following operations.
-		if (checkOperator(s[i]))
+		if (check_operator(s[i]))
 		{
 			# Extract two last elements of the vector
-			num1 = as.integer(pop(storeVector))
-			num2 = as.integer(pop(storeVector))
+			num1 = as.integer(pop(store_vector))
+			num2 = as.integer(pop(store_vector))
 			
 			# Perform the operations
-			output = executeTasks(s[i], num1, num2)
+			output = perform_operation(s[i], num1, num2)
 			
 			# Pushes back the output to the vector
-			push(storeVector,output)	
+			push(store_vector,output)	
 		}
 		# Check wheather the string is numeric or not.
-		else if (checkDigit(s[i]))
+		else if (check_digit(s[i]))
 		{
 		#pushes num back to the vector
-		push(storeVector,s[i])
+		push(store_vector,s[i])
 		}	else {
 		  stop("This is not valid postfix expression.")
 		}
 	}
 	#returns the final one integer output
-	return(paste("The result is:", pop(storeVector)))
+	return(paste("The result is:", pop(store_vector)))
 }
 
 
-push <- function(x, values) {
-  assign(as.character(substitute(x)), c(x, values), parent.frame())
+push <- function(input_vector, values) {
+  assign(as.character(substitute(input_vector)), c(input_vector, values), parent.frame())
   invisible()
   }
 
-pop <- function(x) {
-  if(length(x) == 0) {
+pop <- function(input_vector) {
+  if(length(input_vector) == 0) {
     return(NA)
   }
-  popret <- x[length(x)]
-  assign(as.character(substitute(x)), x[-length(x)], parent.frame())
-  return(popret)
+  pop_output <- input_vector[length(input_vector)]
+  assign(as.character(substitute(input_vector)), input_vector[-length(input_vector)], parent.frame())
+  return(pop_output)
 }

537b122d8e11bfeb7abe174486b9696741e9ffd4
Som Bohora <som-bohora@ouhsc.edu>
2018-02-15 15:16:26 -0600

    add more functions

---
+++ R/get-mode.R
@@ -0,0 +1,5 @@
+get_mode <- function(x) {
+  ux <- unique(x)
+  ux <- x[!is.na(x)]
+  ux[which.max(tabulate(match(x, ux)))]
+}
---
+++ R/summarize-factor.R
@@ -0,0 +1,20 @@
+summarize_factor <- function(ds, column){
+  d1 <- ds[,column]
+  d <- d1[complete.cases(d1),]
+  names_d <- names(d)
+  d %>%
+  dplyr::select_(.dots = names_d) %>%
+  dplyr::group_by_(.dots = names(d)
+  ) %>%
+  dplyr:: summarize(
+    n = n()
+   ) %>%
+  dplyr::mutate(
+    Percentage = paste0(round(n/sum(n)*100, 2), "%")
+   ) %>%
+  knitr::kable(
+    align   = c("r"),
+    format  = "markdown",
+    col.names = proper_case(names(.))
+  )
+}

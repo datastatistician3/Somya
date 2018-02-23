a9229490f3492e547b1af33ca7dd32ac40eef0b7
Som Bohora <som-bohora@ouhsc.edu>
2018-02-23 10:08:55 -0600

    add palindrome and Fibonacci funcs

---
+++ R/fibonacci.R
@@ -0,0 +1,19 @@
+#' @description Computes a Fibonacci number.
+#' @param n an integer
+#' 
+#' @return the nth Fibonacci number
+#' 
+#' @example 
+#' compute_fibonacci(5)
+#' n = 10
+#' for (i in 0:(n-1)) {
+#'   print(compute_fibonacci(i))
+#' }
+
+compute_fibonacci <- function(n) {
+   if (n <= 1){
+     return(n)
+   } else {
+     return(compute_fibonacci(n - 1) + compute_fibonacci(n - 2))
+   }
+}
---
+++ R/palindrome.R
@@ -0,0 +1,35 @@
+
+#' Tests whether a string or sentence is a palindrome. A palindrome
+#' is equal to its reverse, for example "rotor" or "racecar".
+#' @param str a string
+#' @return TRUE if s is a palindrome and FALSE otherwise
+
+
+is_palindrome <- function(str) {
+
+  s <- gsub("\\s", "", tolower(str))
+  s <- substring(s, seq(1,nchar(s),1), seq(1,nchar(s),1))
+
+  #Check for any punctuation characters
+  invalid_chars <- grepl("[[:punct:]]", s, ignore.case = T)
+
+  if (sum(invalid_chars) > 0) {
+    stop("Please remove any punctuation characters.")
+  }
+  #Separate case for shortest strings.
+  if (length(s) <= 1) {
+    return(TRUE)
+  }
+  # Check if first and last characters are same
+  first = s[1]
+  last = s[length(s)]
+
+  if (first == last){
+    shorter = paste0(s[3:length(s)-1], collapse = "")
+    return(is_palindrome(shorter))
+  } else {
+    return(FALSE)
+  }
+}
+
+is_palindrome("Was It A Rat I Saw.")

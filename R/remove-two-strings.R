7496d025d35dc65713b15b27b9aebd06d7e35969
Som Bohora <som-bohora@ouhsc.edu>
2018-04-06 15:22:18 -0500

    amend parameter name

--- R/remove-two-strings.R
+++ R/remove-two-strings.R
@@ -2,7 +2,7 @@ y <- "hi"
 z <-  "hello"
 x <- "hi there hello som"
 
-remove_two_strings <- function(original_str, first_string, second_string){
+remove_two_strings <- function(original_string, first_string, second_string){
   s <- base::trimws(base::gsub(first_string, " ", original_string))
   t <- base::trimws(base::gsub(second_string, " ", s))
   u <- base::gsub("\\s+", " ", t)

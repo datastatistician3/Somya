cb6b1a1002d76752f1a14f20e4806a9eb7bd4380
Som Bohora <som-bohora@ouhsc.edu>
2018-04-10 10:04:55 -0500

    additional remove string func to work in vector

--- R/remove-two-strings.R
+++ R/remove-two-strings.R
@@ -3,6 +3,8 @@ z <-  "hello"
 x <- "hi there hello som"
 
 ## todo use for loop with parameters to allow as many string as possible
+
+#Does not work in vector
 remove_two_strings <- function(original_string, first_string, second_string){
   s <- base::trimws(base::gsub(first_string, " ", original_string))
   t <- base::trimws(base::gsub(second_string, " ", s))
@@ -11,3 +13,20 @@ remove_two_strings <- function(original_string, first_string, second_string){
 }
 
 remove_two_strings(x, y, z)
+
+
+first_string <- c("hi")
+second_string <- c("hello")
+original_string <- c("hi hello there hello som", "hi som there hello")
+
+ds <- data.frame(original_string,first_string,second_string, stringsAsFactors = FALSE)
+
+# Works in vector
+remove_two_strings_vector <- function(original_string, first_string, second_string){
+  s <- base::trimws(str_replace(original_string, first_string, " "))
+  t <- base::trimws(str_replace(s, second_string, " "))
+  u <- base::trimws(str_replace(t, "\\s+", " "))
+  return(u)
+}
+
+remove_two_strings_vector(original_string = tolower(ds$original_string), first_string = ds$first_string,second_string = ds$second_string)

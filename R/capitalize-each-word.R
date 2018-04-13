0827941cf35d30a52687a14ee98b29ff41378c8e
Som Bohora <som-bohora@ouhsc.edu>
2018-04-13 13:52:10 -0500

    generalize capitalize_each_word func

--- R/capitalize-each-word.R
+++ R/capitalize-each-word.R
@@ -6,8 +6,10 @@
 #'
 #' @description This function capitalizes the first letter of each word in a string to uppercase
 #'
-#' @param x A string object or a vector of string objects with or without . or _ or space
+#' @param string A string object or a vector of string objects with or without . or _ or space
 #' delimiters 
+#' 
+#' @param pattern A regular expression pattern to split the string with. Default delimiter is space. 
 #'
 #' @return A string with first letter of each word capitalized
 #'
@@ -15,16 +17,25 @@
 #'
 #' @examples
 #' 
-#'capitalize_each_word("make each letter in this string capitalized.")
-#'capitalize_each_word("make_each _etter_in_this_string_capitalized.")
-#'capitalize_each_word("make.each.letter.in.this.string.cpitalized.")
+#'capitalize_each_word("make each letter in this string capitalized.", pattern = NULL)
+#'capitalize_each_word("make_each _etter_in_this_string_capitalized.", pattern = "[_]")
+#'capitalize_each_word("make.each.letter.in.this.string.cpitalized.", pattern = "[.]")
 
-capitalize_each_word <- function(x){
-  g <- l <- j <- list()
-  g <- stringr::str_split(x, pattern = "[_|.| ]")
-  for ( i in 1:length(g)){
-    l[[i]] <- paste(Hmisc::capitalize(g[[i]]), collapse = " ")
-    j[[i]] <- paste(l[[i]], collapse = " ")
+capitalize_each_word <- function(string, pattern = NULL){
+  if (!is.null(pattern)) {
+    g <- l <- j <- list()
+    g <- stringr::str_split(base::trimws(string), pattern = pattern)
+    for ( i in 1:length(g)){
+      l[[i]] <- paste(Hmisc::capitalize(g[[i]]), collapse = " ")
+      j[[i]] <- paste(l[[i]], collapse = " ")
+    }
+    } else {
+    g <- l <- j <- list()
+    g <- stringr::str_split(base::trimws(string), pattern = "\\s+")
+    for ( i in 1:length(g)){
+      l[[i]] <- paste(Hmisc::capitalize(g[[i]]), collapse = " ")
+      j[[i]] <- paste(l[[i]], collapse = " ")
+  }
 }
   return(unlist(j))
 }

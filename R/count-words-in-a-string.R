7b1472b8333c26d96fe5dd0ffcd51b016ed58fe6
Som Bohora <som-bohora@ouhsc.edu>
2018-05-18 16:30:58 -0500

    add python func

--- R/count-words-in-a-string.R
+++ R/count-words-in-a-string.R
@@ -14,3 +14,20 @@ count_words_in_string <- function(string){
 
 string = "This is a string. This is a a word. hora?"
 count_words_in_string(string = string)
+
+
+# In python
+string = "This is a string. This is a a word."
+
+def count_words(x):
+    counts = dict()
+    s = string.split(' ')
+    
+    for word in s:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1 
+    return(counts)
+    
+print(count_words(string))

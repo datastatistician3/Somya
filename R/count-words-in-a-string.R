9307fada7eb7a2f56611e3515e151e870bd19c7b
Som Bohora <som-bohora@ouhsc.edu>
2018-05-22 15:42:31 -0500

    comment out python func

--- R/count-words-in-a-string.R
+++ R/count-words-in-a-string.R
@@ -17,17 +17,15 @@ count_words_in_string(string = string)
 
 
 # In python
-string = "This is a string. This is a a word."
-
-def count_words(x):
-    counts = dict()
-    s = string.split(' ')
-    
-    for word in s:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1 
-    return(counts)
-    
-print(count_words(string))
+# def count_words(x):
+#     counts = dict()
+#     s = string.split(' ')
+#     
+#     for word in s:
+#         if word in counts:
+#             counts[word] += 1
+#         else:
+#             counts[word] = 1 
+#     return(counts)
+#     
+# print(count_words(string))

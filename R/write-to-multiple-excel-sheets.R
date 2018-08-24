81608c016e573d970daa424513f1cdc741579f46
Som Bohora <som-bohora@ouhsc.edu>
2018-08-24 11:26:52 -0500

    rbind DFs

--- R/write-to-multiple-excel-sheets.R
+++ R/write-to-multiple-excel-sheets.R
@@ -17,3 +17,27 @@
 # }
 # 
 # write_different_sheets(df_list = df_list)
+
+
+# df1 <- mtcars[1:5,]
+# df2 <- mtcars[6:10,]
+# df3 <- mtcars[11:15,]
+# 
+# getDF <- function(x, num = 5){
+#   return(x[1:num,])
+# }
+# 
+# final_df <- NULL
+# for ( i in 1:2){
+#   x <- getDF(mtcars,i)
+#   final_df <-  rbind.data.frame(out, x)
+# }
+# 
+# final_df <- NULL
+# for (i in 1:length(db_list)) {
+#   x <- getJSOAP(db_location[i],db_name[i])
+#   final_df <- rbind.data.frame(final_df, x)
+# }
+# 
+# myList <- list(getJSOAP(db_location[1],db_name[1]), getJSOAP(db_location[2],db_name[2]))
+# do.call(rbind.data.frame, myList)
